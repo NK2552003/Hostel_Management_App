1040ee0b39d5c31aa97832e7cf96e693dfd8d292
Nitish <nk2552003@gmail.com>
2024-11-19 14:12:16 +0530

    mdfied

--- lib/screens/Dashboards/hostler_dashboard.dart
+++ lib/screens/Dashboards/hostler_dashboard.dart
@@ -1,5 +1,5 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // For SystemUiOverlayStyle
 import 'package:hostel_management_app/components/custom_drawer.dart';
 import 'package:hostel_management_app/components/event_list.dart';
 import 'package:hostel_management_app/components/fee_overview.dart';
@@ -12,82 +12,94 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Set system overlay style
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent, // Make status bar transparent
+        statusBarIconBrightness:
+            Brightness.dark, // Set status bar icons to black
+        statusBarBrightness: Brightness.light, // For iOS compatibility
+      ),
+    );
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(60), // Reduced height
-        child: Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10, top: 50),
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.green.shade900, Colors.green.shade900],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-              borderRadius:
-                  BorderRadius.circular(20), // Circular from all sides
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.green.withOpacity(0.4),
-                  blurRadius: 10,
-                  offset: Offset(0, 4), // Shadow below the AppBar
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.green.shade900, Colors.green.shade900],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                 ),
-              ],
-            ),
-            child: SizedBox(
-              height: 60,
-              child: Stack(
-                alignment: Alignment.center, // Align content to the center
-                children: [
-                  // Centered title
-                  const Text(
-                    'Dashboard',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  // Left-aligned menu icon
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Builder(
-                      builder: (BuildContext context) {
-                        return IconButton(
-                          icon: const Icon(Icons.menu, color: Colors.white),
-                          onPressed: () {
-                            Scaffold.of(context).openDrawer();
-                          },
-                        );
-                      },
-                    ),
-                  ),
-                  // Right-aligned actions
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.warning_amber_rounded,
-                              color: Colors.white),
-                          onPressed: () {
-                            // Handle emergency action
-                          },
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.notifications,
-                              color: Colors.white),
-                          onPressed: () {
-                            // Handle notifications action
-                          },
-                        ),
-                        const SizedBox(width: 5),
-                      ],
-                    ),
+                borderRadius:
+                    BorderRadius.circular(20), // Circular from all sides
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.green.withOpacity(0.4),
+                    blurRadius: 10,
+                    offset: Offset(0, 4), // Shadow below the AppBar
                   ),
                 ],
+              ),
+              child: SizedBox(
+                height: 60,
+                child: Stack(
+                  alignment: Alignment.center, // Align content to the center
+                  children: [
+                    // Centered title
+                    const Text(
+                      'Dashboard',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    // Left-aligned menu icon
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Builder(
+                        builder: (BuildContext context) {
+                          return IconButton(
+                            icon: const Icon(Icons.menu, color: Colors.white),
+                            onPressed: () {
+                              Scaffold.of(context).openDrawer();
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                    // Right-aligned actions
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.warning_amber_rounded,
+                                color: Colors.white),
+                            onPressed: () {
+                              // Handle emergency action
+                            },
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.notifications,
+                                color: Colors.white),
+                            onPressed: () {
+                              // Handle notifications action
+                            },
+                          ),
+                          const SizedBox(width: 5),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
